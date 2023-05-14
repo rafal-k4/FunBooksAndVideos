7a53ee4a2036140d60b68539a02bfeb0b61b3b2c
Rafał Kapuściński <kapuscinski.rafal@wp.pl>
2023-05-14 14:20:39 +0200

    add ardalis clean endpoints with Purchase order endpoint

---
+++ FunBooksAndVideos_eCommerceShop/Endpoints/PurchaseOrder/Create.CreatePurchaseOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FunBooksAndVideos_eCommerceShop.Endpoints.PurchaseOrder;
+
+public class CreatePurchaseOrderRequest
+{
+    public int Purchaseorder { get; set; }
+    public decimal Total { get; set; }
+    public int Customer { get; set; }
+    public List<string> ItemLines { get; set; } = null!;
+}
